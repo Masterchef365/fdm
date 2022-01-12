d20b8540f80dd35fc23a823dbde64db2948d1b9a
Masterchef365 <duncan.freeman1@gmail.com>
2022-01-11 23:14:53 -0800

    First simulation!

--- examples/string.rs
+++ examples/string.rs
@@ -1,64 +1,93 @@
+use fdm::fdm;
 use idek::{prelude::*, IndexBuffer};
 
 fn main() -> Result<()> {
-    launch::<_, TriangleApp>(Settings::default().vr_if_any_args())
+    let dt = 0.01;
+    let init = [1., 1., 1., 1., 1., 0., 0., 0., 0., 0., 0., 0.];
+    let time_len = 500;
+    let x_len = init.len();
+
+    let mesh = fdm(10., dt * time_len as f32, 1.0, x_len, time_len, &init);
+
+    let sim = SimData {
+        x_len,
+        time_len,
+        data: mesh,
+    };
+
+    launch::<SimData, TriangleApp>(Settings::default().vr_if_any_args().args(sim))
 }
 
-const QUAD_VERTS: [Vertex; 4] = [
-    Vertex {
-        pos: [-1., -1., 0.],
-        color: [0., 0., 0.],
-    },
-    Vertex {
-        pos: [1., -1., 0.],
-        color: [1., 0., 0.],
-    },
-    Vertex {
-        pos: [1., 1., 0.],
-        color: [1., 1., 0.],
-    },
-    Vertex {
-        pos: [-1., 1., 0.],
-        color: [0., 1., 0.],
-    },
-];
-
-const QUAD_INDICES: [u32; 12] = [
-    // Facing toward the camera
-    3, 1, 0, 3, 2, 1, // Facing away
-    0, 1, 3, 1, 2, 3,
-];
+#[derive(Default)]
+pub struct SimData {
+    pub x_len: usize,
+    pub time_len: usize,
+    pub data: Vec<f32>,
+}
 
 struct TriangleApp {
     verts: VertexBuffer,
     indices: IndexBuffer,
+    line_shader: Shader,
+
+    frame: usize,
+    sim: SimData,
 }
 
-impl App for TriangleApp {
-    fn init(ctx: &mut Context, _: &mut Platform, _: ()) -> Result<Self> {
-        let verts = ctx.vertices(&QUAD_VERTS, false)?;
-        let indices = ctx.indices(&QUAD_INDICES, false)?;
+impl App<SimData> for TriangleApp {
+    fn init(ctx: &mut Context, _: &mut Platform, sim: SimData) -> Result<Self> {
+        let vertices = sim_vertices(&sim, 0);
+
+        let verts = ctx.vertices(&vertices, true)?;
+        let indices: Vec<u32> = (1..vertices.len() * 2 - 1)
+            .map(|i| (i / 2) as u32)
+            .collect();
+        let indices = ctx.indices(&indices, false)?;
 
         Ok(Self {
+            line_shader: ctx.shader(
+                 DEFAULT_VERTEX_SHADER,
+                 DEFAULT_FRAGMENT_SHADER,
+                 Primitive::Lines,
+            )?,
             verts,
             indices,
+            sim,
+            frame: 0,
         })
     }
 
-    fn frame(&mut self, _ctx: &mut Context, _: &mut Platform) -> Result<Vec<DrawCmd>> {
-        Ok(vec![DrawCmd::new(self.verts)
-            .indices(self.indices)])
+    fn frame(&mut self, ctx: &mut Context, _: &mut Platform) -> Result<Vec<DrawCmd>> {
+        self.frame = (self.frame + 1) % self.sim.time_len;
+        
+        let vertices = sim_vertices(&self.sim, self.frame);
+        ctx.update_vertices(self.verts, &vertices)?;
+
+        Ok(vec![DrawCmd::new(self.verts).indices(self.indices).shader(self.line_shader)])
     }
 
-    fn event(
-        &mut self,
-        ctx: &mut Context,
-        platform: &mut Platform,
-        event: Event,
-    ) -> Result<()> {
+    fn event(&mut self, ctx: &mut Context, platform: &mut Platform, event: Event) -> Result<()> {
         idek::simple_ortho_cam_ctx(ctx, platform);
         idek::close_when_asked(platform, &event);
         Ok(())
     }
+}
 
+fn sim_vertices(sim: &SimData, t: usize) -> Vec<Vertex> {
+    let row = sim
+        .data
+        .chunks_exact(sim.x_len)
+        .skip(t)
+        .next()
+        .expect("Invalid row");
+
+    let x_map = |i: usize| (i as f32 / sim.x_len as f32) * 2. - 1.;
+
+    row.iter()
+        .enumerate()
+        .map(|(i, u)| Vertex {
+            pos: [x_map(i), -*u, 0.],
+            color: [1.; 3],
+        })
+        .collect()
 }

294aabc76fb7c21b0e370f55c3e6f299149e2c1c
Masterchef365 <duncan.freeman1@gmail.com>
2022-03-26 15:59:42 -0700

    Blows up

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,55 @@
 use num_complex::Complex32;
 
-fn zero() -> Complex32 {
+const fn zero() -> Complex32 {
     Complex32::new(0., 0.)
+}
+
+pub struct Fdm {
+    last: Vec<Complex32>,
+    current: Vec<Complex32>,
+    dx: f32,
+    h: f32,
+    m: f32,
+}
+
+impl Fdm {
+    /// Creates a new FDM with the given width (in the X direction) and initial grid contents `init`
+    pub fn new(init: &[Complex32], width: f32, h: f32, m: f32) -> Self {
+        Self {
+            current: init.to_vec(),
+            last: vec![zero(); init.len()],
+            dx: width / init.len() as f32,
+            h,
+            m,
+        }
+    }
+
+    pub fn step(&mut self, dt: f32, v: impl Fn(f32) -> Complex32) {
+        std::mem::swap(&mut self.last, &mut self.current);
+        for ((idx, current), last) in self
+            .current
+            .iter_mut()
+            .enumerate()
+            .skip(1)
+            .zip(self.last.windows(3))
+        {
+            let x = idx as f32 * self.dx;
+
+            let cfd = central_finite_difference((last[0], last[1], last[2]));
+
+            *current = last[1]
+                + (dt / (self.dx * self.dx)) * (self.h / (2. * self.m)) * Complex32::i() * cfd
+                - dt / self.h * Complex32::i() * v(x) * last[1];
+        }
+    }
+
+    pub fn grid(&self) -> &[Complex32] {
+        &self.current
+    }
+
+    pub fn dx(&self) -> f32 {
+        self.dx
+    }
 }
 
 // TODO:
@@ -13,7 +61,14 @@ fn zero() -> Complex32 {
 /// n_x: Number of discrete spatial steps
 /// n_t: Number of discrete time steps
 /// init: Initial condition at t = 0
-pub fn fdm(max_x: f32, max_t: f32, c: f32, n_x: usize, n_t: usize, init: &[Complex32]) -> Vec<Complex32> {
+pub fn nope(
+    max_x: f32,
+    max_t: f32,
+    c: f32,
+    n_x: usize,
+    n_t: usize,
+    init: &[Complex32],
+) -> Vec<Complex32> {
     // Input checks
     assert_eq!(init.len(), n_x);
 
@@ -26,33 +81,12 @@ pub fn fdm(max_x: f32, max_t: f32, c: f32, n_x: usize, n_t: usize, init: &[Compl
     let mut mesh = vec![zero(); n_x * n_t];
     mesh[..n_x].as_mut().copy_from_slice(init);
 
-    /// Find the three x neighboring pixels
-    fn neighborhood(row: &[Complex32], i: usize) -> (Complex32, Complex32, Complex32) {
-        let left = match i.checked_sub(1) {
-            Some(q) => row[q],
-            None => zero(),
-        };
-
-        let center = row[i];
-
-        let right = match i + 1 < row.len() {
-            true => row[i + 1],
-            false => zero(),
-        };
-
-        (left, center, right)
-    }
-
-    /// Compute the Central Finite Distance from the given neighborhood
-    fn central_finite_difference((left, center, right): (Complex32, Complex32, Complex32)) -> Complex32 {
-        left + -2. * center + right
-    }
-
     // Fill in the second time step special case
     let (mesh_t_0, mesh_t_1) = mesh[..n_x * 2].split_at_mut(n_x);
     for (i, u) in mesh_t_1.iter_mut().enumerate() {
         let neighbors @ (_, center, _) = neighborhood(&mesh_t_0, i);
-        *u = center - 0.5 * courant * courant * central_finite_difference(neighbors) * Complex32::i();
+        *u = center
+            - 0.5 * courant * courant * central_finite_difference(neighbors) * Complex32::i();
     }
 
     // Compute the remaining time steps
@@ -66,9 +100,35 @@ pub fn fdm(max_x: f32, max_t: f32, c: f32, n_x: usize, n_t: usize, init: &[Compl
         for (i, u) in current_row.iter_mut().enumerate() {
             let down = prev_prev_row[i];
             let neighbors @ (_, center, _) = neighborhood(prev_row, i);
-            *u = -down + 2. * center + courant * courant * central_finite_difference(neighbors) * Complex32::i();
+            *u = -down
+                + 2. * center
+                + courant * courant * central_finite_difference(neighbors) * Complex32::i();
         }
     }
 
     mesh
+}
+
+/// Find the three x neighboring pixels
+fn neighborhood(row: &[Complex32], i: usize) -> (Complex32, Complex32, Complex32) {
+    let left = match i.checked_sub(1) {
+        Some(q) => row[q],
+        None => zero(),
+    };
+
+    let center = row[i];
+
+    let right = match i + 1 < row.len() {
+        true => row[i + 1],
+        false => zero(),
+    };
+
+    (left, center, right)
+}
+
+/// Compute the Central Finite Distance from the given neighborhood
+fn central_finite_difference(
+    (left, center, right): (Complex32, Complex32, Complex32),
+) -> Complex32 {
+    left + -2. * center + right
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,105 @@
-// TODO: Show a 2D animation of the FDM at work!
-use fdm::fdm;
+use fdm::Fdm;
+use idek::{prelude::*, IndexBuffer};
+use num_complex::Complex32;
 
-fn main() {
-    let init = [1., 1., 1., 1., 1., 0., 0., 0., 0., 0., 0., 0.];
-    let dt = 0.1;
-    let n_t = 500;
+fn main() -> Result<()> {
+    /*
+    let dt = 0.01;
+    let x_len = 1000;
 
-    let mesh = fdm(10., dt * n_t as f32, 1.0, init.len(), n_t, &init);
+    let time_len = 8000;
 
-    for row in mesh.chunks_exact(init.len()) {
-        for item in row {
-            print!("{:+.03}, ", item);
-        }
-        println!();
+    let sim = SimData {
+        x_len,
+        time_len,
+        data: mesh,
+    };
+    */
+
+    launch::<(), FdmVisualizer>(Settings::default().vr_if_any_args())
+}
+
+struct FdmVisualizer {
+    verts: VertexBuffer,
+    indices: IndexBuffer,
+    line_shader: Shader,
+
+    fdm: Fdm,
+
+    camera: MultiPlatformCamera,
+}
+
+impl App for FdmVisualizer {
+    fn init(ctx: &mut Context, platform: &mut Platform, _: ()) -> Result<Self> {
+        let n_cells = 10;
+        let init: Vec<Complex32> = (0..n_cells)
+            .map(|x| {
+                //if (x >= x_len / 3) && (x <= 2 * x_len / 3) {
+                if x == n_cells / 2 {
+                    Complex32::new(1.0, 0.)
+                } else {
+                    Complex32::new(0., 0.)
+                }
+            })
+            .collect();
+
+        let fdm = Fdm::new(&init, 1., 1., 1.);
+
+        let vertices = fdm_vertices(&fdm);
+
+        let verts = ctx.vertices(&vertices, true)?;
+        let indices: Vec<u32> = (1..vertices.len() * 2 - 1)
+            .map(|i| (i / 2) as u32)
+            .collect();
+        let indices = ctx.indices(&indices, false)?;
+
+        Ok(Self {
+            fdm,
+            line_shader: ctx.shader(
+                DEFAULT_VERTEX_SHADER,
+                DEFAULT_FRAGMENT_SHADER,
+                Primitive::Lines,
+            )?,
+            verts,
+            indices,
+            camera: MultiPlatformCamera::new(platform),
+        })
     }
+
+    fn frame(&mut self, ctx: &mut Context, _: &mut Platform) -> Result<Vec<DrawCmd>> {
+        self.fdm.step(0.01, |x: f32| Complex32::new(x, 0.));
+
+        let vertices = fdm_vertices(&self.fdm);
+        ctx.update_vertices(self.verts, &vertices)?;
+
+        Ok(vec![DrawCmd::new(self.verts)
+            .indices(self.indices)
+            .shader(self.line_shader)])
+    }
+
+    fn event(
+        &mut self,
+        ctx: &mut Context,
+        platform: &mut Platform,
+        mut event: Event,
+    ) -> Result<()> {
+        if self.camera.handle_event(&mut event) {
+            ctx.set_camera_prefix(self.camera.get_prefix())
+        }
+        idek::close_when_asked(platform, &event);
+        Ok(())
+    }
+}
+
+fn fdm_vertices(fdm: &Fdm) -> Vec<Vertex> {
+    let x_map = |i: usize| (i as f32 * fdm.dx()) * 2. - 1.;
+
+    fdm.grid()
+        .iter()
+        .enumerate()
+        .map(|(i, u)| Vertex {
+            pos: [x_map(i), -u.re, u.im],
+            color: [1.; 3],
+        })
+        .collect()
 }

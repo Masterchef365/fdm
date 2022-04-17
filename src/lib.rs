bbec532264b0fd63118181359f8d995e39fd4b4f
Masterchef365 <duncan.freeman1@gmail.com>
2022-04-17 01:28:57 -0700

    Rayon, why not

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod array2d;
 use num_complex::Complex32;
 pub type Array2D = array2d::Array2D<Complex32>;
+use rayon::prelude::*;
 
 const fn zero() -> Complex32 {
     Complex32::new(0., 0.)
@@ -25,28 +26,36 @@ impl Fdm {
     }
 
     pub fn step(&mut self, dt: f32, _v: impl Fn(f32) -> Complex32) {
-        let (nx, ny) = inner_size(&self.current);
+        let (nx, _ny) = inner_size(&self.current);
 
         std::mem::swap(&mut self.last, &mut self.current);
         const K: usize = 40;
 
         // here hbar / 2m = 1
-        let r = -Complex32::i() * dt;// / (self.dx * self.dx);
+        let r = -Complex32::i() * dt; // / (self.dx * self.dx);
 
+        let width = self.scratch.width();
+        let inner_height = self.scratch.height() - 2;
         for _ in 0..K {
-            for i in 1..=nx {
-                for j in 1..=ny {
-                    let sum = self.current[(i - 1, j)]
-                        + self.current[(i + 1, j)]
-                        + self.current[(i, j - 1)]
-                        + self.current[(i, j + 1)];
+            self.scratch
+                .data_mut()
+                .par_chunks_exact_mut(width)
+                .enumerate()
+                .skip(1)
+                .take(inner_height)
+                .for_each(|(j, row)| {
+                    for i in 1..=nx {
+                        let sum = self.current[(i - 1, j)]
+                            + self.current[(i + 1, j)]
+                            + self.current[(i, j - 1)]
+                            + self.current[(i, j + 1)];
 
-                    self.scratch[(i, j)] = (self.last[(i, j)] - r * sum) / (1. - 4. * r);
-                }
-            }
-
+                        row[i] = (self.last[(i, j)] - r * sum) / (1. - 4. * r);
+                    }
+                });
             std::mem::swap(&mut self.current, &mut self.scratch);
         }
+
     }
 
     pub fn grid(&self) -> &Array2D {
